ed5108222e8c4ba3abb88038a4250e22167ddd6f
Brunito2017 <brunomontupil0@gmail.com>
2026-04-14 12:18:49 -0400

    feat: add Database class for database connection management

---
+++ app/config/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database
+{
+	private static $host = 'localhost';
+	private static $port = '5432';
+	private static $dbname = 'houseware_db'; 
+	private static $user = 'admin';    
+	private static $password = 'xa31_S'; 
+	private static $pdo = null;
+
+
+	public static function getConnection(): \PDO
+	{
+		if (self::$pdo === null) {
+			$dsn = sprintf(
+				'pgsql:host=%s;port=%s;dbname=%s',
+				self::$host,
+				self::$port,
+				self::$dbname
+			);
+			try {
+				self::$pdo = new \PDO($dsn, self::$user, self::$password, [
+					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+					\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+				]);
+			} catch (\PDOException $e) {
+				die('Error de conexión a la base de datos: ' . $e->getMessage());
+			}
+		}
+		return self::$pdo;
+	}
+}
